e09a7e646bff70f3b4ecfbc285fb042c5af2d89e
PD Jell <jellive7@gmail.com>
2020-03-03 18:23:11 +0900

    add Apicontext.

--- src/User.tsx
+++ src/User.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import axios from 'axios'
-import { useAsync } from 'react-async'
+import React, { useEffect } from 'react'
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext'
 
-async function getUser({ id }: any): Promise<any> { // useAsync를 쓸 때에는 파라미터를 객체로 지정해야 한다.
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-}
 function User({ id }: { id: number }) {
-  const { data: user, error, isLoading } = useAsync({
-    promiseFn: getUser,
-    id,
-    watch: id // Vue의 watch와 같음. 변경되면 함수를 다시 부름.
-  })
+  const state = useUsersState()
+  const dispatch = useUsersDispatch()
+  useEffect(() => {
+    getUser(dispatch, id)
+  }, [dispatch, id])
 
-  if (isLoading) return <div>로딩중..</div>;
+  const { data: user, loading, error } = state.user;
+
+  if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
--- src/Users.tsx
+++ src/Users.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import axios from 'axios'
-import { useAsync } from 'react-async'
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext'
 import User from './User'
 
 
@@ -10,22 +9,23 @@ type User = {
     name: string
 }
 
-async function getUsers() {
-    const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-    );
-    return response.data;
-}
-
 function Users() {
     const [userId, setUserId] = useState(0)
     // react hook 규칙에 따르면 커스텀 훅은 capitalized가 되어야 한다. (via https://stackoverflow.com/questions/55846641/react-hook-usestate-is-called-in-function-app-which-is-neither-a-react-funct)
-    const { data: users, error, isLoading, run } = useAsync({ deferFn: getUsers }) // react-async에서 사용자의 액션에 따라 부르고 싶을 때에는 promiseFn이 아닌 deferFn을 넣는다.
+    // const { data: users, error, isLoading, run } = useAsync({ deferFn: getUsers }) // react-async에서 사용자의 액션에 따라 부르고 싶을 때에는 promiseFn이 아닌 deferFn을 넣는다.
 
+    const state = useUsersState()
+    const dispatch = useUsersDispatch()
 
-    if (isLoading) return <div>로딩중..</div>;
+    const { data: users, loading, error } = state.users
+
+    const fetchData = () => {
+        getUsers(dispatch);
+    };
+
+    if (loading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return <button onClick={run}>불러오기</button>;
+    if (!users) return <button onClick={fetchData}>불러오기</button>;
     return (
         <>
             <ul>
@@ -38,7 +38,7 @@ function Users() {
           </li>
                 ))}
             </ul>
-            <button onClick={run}>다시 불러오기</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
