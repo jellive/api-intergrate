2627beb3b084065ca25ed0e65c2d190bfff645ba
PD Jell <jellive7@gmail.com>
2020-03-03 17:38:22 +0900

    add comment.

--- src/Users.tsx
+++ src/Users.tsx
@@ -20,7 +20,7 @@ async function getUsers() {
 function Users() {
     const [userId, setUserId] = useState(0)
     // react hook 규칙에 따르면 커스텀 훅은 capitalized가 되어야 한다. (via https://stackoverflow.com/questions/55846641/react-hook-usestate-is-called-in-function-app-which-is-neither-a-react-funct)
-    const { data: users, error, isLoading, run } = useAsync({ deferFn: getUsers })
+    const { data: users, error, isLoading, run } = useAsync({ deferFn: getUsers }) // react-async에서 사용자의 액션에 따라 부르고 싶을 때에는 promiseFn이 아닌 deferFn을 넣는다.
 
 
     if (isLoading) return <div>로딩중..</div>;
