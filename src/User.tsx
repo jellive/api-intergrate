609526e4fe136b123dc354c11e797297322b4a65
PD Jell <jellive7@gmail.com>
2020-03-03 17:37:17 +0900

    add react-async.

--- src/User.tsx
+++ src/User.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import axios from 'axios'
-import useAsync from './useAsync'
+import { useAsync } from 'react-async'
 
-async function getUser(id: number) {
+async function getUser({ id }: any): Promise<any> { // useAsync를 쓸 때에는 파라미터를 객체로 지정해야 한다.
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
   return response.data;
 }
-
 function User({ id }: { id: number }) {
-  const [state] = useAsync(() => getUser(id), [id])
-  const { loading, data: user, error } = state
+  const { data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id // Vue의 watch와 같음. 변경되면 함수를 다시 부름.
+  })
 
-
-  if (loading) return <div>로딩중..</div>;
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
--- src/Users.tsx
+++ src/Users.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import UseAsync from './useAsync'
+import { useAsync } from 'react-async'
 import User from './User'
 
 
@@ -20,13 +20,12 @@ async function getUsers() {
 function Users() {
     const [userId, setUserId] = useState(0)
     // react hook 규칙에 따르면 커스텀 훅은 capitalized가 되어야 한다. (via https://stackoverflow.com/questions/55846641/react-hook-usestate-is-called-in-function-app-which-is-neither-a-react-funct)
-    const [state, refetch] = UseAsync(getUsers, [], true)
+    const { data: users, error, isLoading, run } = useAsync({ deferFn: getUsers })
 
-    const { loading, data: users, error } = state
 
-    if (loading) return <div>로딩중..</div>;
+    if (isLoading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return <button onClick={refetch}>불러오기</button>;
+    if (!users) return <button onClick={run}>불러오기</button>;
     return (
         <>
             <ul>
@@ -39,7 +38,7 @@ function Users() {
           </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={run}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
